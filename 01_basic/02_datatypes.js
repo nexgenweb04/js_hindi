0c53699aa56d62b4b092de697fc0a582ca20b583
nexgenweb04 <nexgenweb04@gmail.com>
2025-03-03 17:13:46 +0000

    DataTypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) we are using node js not browser
+
+// code readability should be high
+
+console.log(3+3) 
+console.log("Saifuddin")
+
+let name = "Saifuddin" // string datatype
+let age = 18
+let isLoggedIn = false
+let state = null
+let country;
+
+
+// number => 2 to power 53
+// bigint 
+// string =>""
+// boolean => true/false
+// null => stanalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);
+
+// typeof undefined = undefined
+// typeof null = object
+
+
