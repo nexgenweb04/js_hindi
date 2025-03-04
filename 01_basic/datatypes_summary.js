44c13442a01adf134caad53841ada44a6eee82b1
nexgenweb04 <nexgenweb04@gmail.com>
2025-03-04 17:45:50 +0000

    datatype_summery

---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 Types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const OutsideTemp = null
+let userEmails;
+const userEmail = undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 3232423424234324234324324324324324324234324324324324324324234324234324324234324324245345464565745887455464353235252435536457488n
+//console.log(bigNumber);
+
+
+
+// Reference ( Non Premitive )
+
+// Array, Objects, Functions
+
+const heros = ["Saif", "Abdus Salam", "Fatima"];
+let myObj = {
+    name: "Sk Md Saifuddin",
+    age: 33
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof id);
